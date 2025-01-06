7ac4cc98bcce89145098440223f56591f6b97a5d
Dildor <dildormingturaeva@gmail.com>
2025-01-06 17:26:53 +0500

    Register

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .schema import swagger_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/users/', include('users.urls')),
 ]
+
 
 urlpatterns += swagger_urlpatterns
 
